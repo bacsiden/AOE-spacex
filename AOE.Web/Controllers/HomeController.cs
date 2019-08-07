4df3cd2b6c2ae20339c4a8accfc6debbfd7d18c3
Monte Cristo <duykhanh.sctn@gmail.com>
2019-08-07 18:26:33 +0700

    xxx

--- AOE.Web/Controllers/HomeController.cs
+++ AOE.Web/Controllers/HomeController.cs
@@ -7,12 +7,20 @@ using Microsoft.AspNetCore.Mvc;
 using AOE.Web.Models;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using Microsoft.AspNetCore.Http;
 
 namespace AOE.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public static class DataName
+        {
+            public const string Content = "content";
+            public const string AboutUs = "about-us";
+            public const string Service = "services";
+        }
         public static dynamic Data = null;
+
         public IActionResult Index()
         {
             return View();
@@ -22,26 +30,20 @@ namespace AOE.Web.Controllers
         {
             return View();
         }
-        public static dynamic GetAboutUs()
+
+        public IActionResult RepaireOverhual()
         {
-            using (StreamReader r = new StreamReader("about-us.json", encoding: System.Text.Encoding.UTF8))
-            {
-                var json = r.ReadToEnd();
-                return JObject.Parse(json);
-            }
+            return View();
         }
 
         public IActionResult Services()
         {
             return View();
         }
-        public static dynamic GetServices()
+
+        public IActionResult QualityAssurance()
         {
-            using (StreamReader r = new StreamReader("services.json", encoding: System.Text.Encoding.UTF8))
-            {
-                var json = r.ReadToEnd();
-                return JObject.Parse(json);
-            }
+            return View();
         }
 
         public IActionResult Contact()
@@ -49,11 +51,54 @@ namespace AOE.Web.Controllers
             return View();
         }
 
+        public IActionResult ChangeLanguage()
+        {
+            var lang = string.Empty;
+            Request.Cookies.TryGetValue("lang", out lang);
+            Response.Cookies.Delete("lang");
+            if (lang == "en")
+                Response.Cookies.Append("lang", "vn");
+            else
+                Response.Cookies.Append("lang", "en");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public static dynamic GetAboutUs()
+        {
+            using (StreamReader r = new StreamReader("data\\about-us.json", encoding: System.Text.Encoding.UTF8))
+            {
+                var json = r.ReadToEnd();
+                return JObject.Parse(json);
+            }
+        }
+        
+        public static dynamic GetServices()
+        {
+            using (StreamReader r = new StreamReader("data\\services.json", encoding: System.Text.Encoding.UTF8))
+            {
+                var json = r.ReadToEnd();
+                return JObject.Parse(json);
+            }
+        }
 
         public static dynamic GetContent()
         {
             //if (Data != null) return Data;
-            using (StreamReader r = new StreamReader("content.json", encoding: System.Text.Encoding.UTF8))
+            using (StreamReader r = new StreamReader("data\\content.json", encoding: System.Text.Encoding.UTF8))
+            {
+                var json = r.ReadToEnd();
+                Data = JObject.Parse(json);
+                return Data;
+            }
+        }
+
+        public static dynamic GetData(HttpRequest request, string dataName)
+        {
+            var lang = string.Empty;
+            request.Cookies.TryGetValue("lang", out lang);
+            lang = lang == "vn" ? "-vn" : null;
+            var path = $"data\\{dataName}{lang}.json";
+            using (StreamReader r = new StreamReader(path, encoding: System.Text.Encoding.UTF8))
             {
                 var json = r.ReadToEnd();
                 Data = JObject.Parse(json);
