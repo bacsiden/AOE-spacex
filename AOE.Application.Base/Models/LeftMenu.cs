03a00ace1a88eac8ab3cb0d587861773fd70b2e2
Monte Cristo <duykhanh.sctn@gmail.com>
2019-11-20 10:53:50 +0700

    layout

--- AOE.Application.Base/Models/LeftMenu.cs
+++ AOE.Application.Base/Models/LeftMenu.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System.Collections.Generic;
 
 namespace AOE.Application.Base.Models
@@ -6,7 +7,10 @@ namespace AOE.Application.Base.Models
     [BsonIgnoreExtraElements]
     public class LeftMenu
     {
+        [BsonId]
+        public ObjectId Id { get; set; }
         public string Title { get; set; }
+        public int Order { get; set; }
         public string Icon { get; set; }
         public string Link { get; set; }
         public List<string> Actions { get; set; }
