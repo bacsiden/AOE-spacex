48e3f62d7dce59cdb93ab094135119d1ab90f3ca
Duy Khánh <duykhanh.sctn@gmail.com>
2021-05-13 17:42:56 +0700

    xxx

---
+++ AOE.Ebvie/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace AOE.Ebvie.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly IEmailSender _emailSender;
+        public HomeController()
+        {
+            //_emailSender = emailSender;
+        }
+
+        public IActionResult Index() => View();
+        public IActionResult InBoard() => View();
+        public IActionResult OutBoard() => View();
+
+        public IActionResult AboutUs() => View();
+
+        public IActionResult OutTeam() => View();
+
+        public IActionResult Investor() => View();
+
+        public IActionResult FAQ() => View();
+        public IActionResult News() => View();
+        public IActionResult Pressroom() => View();
+        public IActionResult Events() => View();
+        public IActionResult Contact() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Contact(string name, string email, string subject, string message)
+        {
+            //await _emailSender.SendEmailAsync("info@taesco.com", $"[{email} - {name}] {subject}", message);
+            return Redirect("/Home/Contact?thanks=1");
+        }
+
+        public static string lang = "-en";
+        public IActionResult ChangeLanguage(string url)
+        {
+            lang = lang == "-vn" ? "-en" : "-vn";
+            return RedirectToAction(url);
+        }
+
+        public static dynamic GetData(HttpRequest request, string dataName)
+        {
+            var path = $"data\\{dataName}{lang}.json";
+            using (StreamReader r = new StreamReader(path, encoding: System.Text.Encoding.UTF8))
+            {
+                var json = r.ReadToEnd();
+                return JObject.Parse(json);
+            }
+        }
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
---
+++ AOE.Ebvie/Startup.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AOE.Ebvie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            Env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; set; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            IMvcBuilder builder = services.AddRazorPages();
+            if (Env.IsDevelopment())
+            {
+                builder.AddRazorRuntimeCompilation();
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
